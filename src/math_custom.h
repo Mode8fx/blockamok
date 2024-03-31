b1a16be779bc06620fef695ba9d60775fb9ab9df
Mode8fx <gateguy33@gmail.com>
2024-03-31 16:25:04 -0400

    Scale FOV relative to aspect ratio
    Wide screen = narrow FOV = difficulty should(?) be the same across all aspect ratios

--- src/math_custom.h
+++ src/math_custom.h
@@ -1,4 +1,8 @@
+#include "./draw.h"
+
 #define FOV_ANGLE 45
+//#define ADJUSTED_FOV (FOV_ANGLE * WIDTH / HEIGHT)
+#define ADJUSTED_FOV (FOV_ANGLE * HEIGHT / WIDTH)
 
 float transform3Dto2D(float xy, float z);
 float randF(float min, float max);
