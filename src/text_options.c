472939cb215f5313c2f0e43494e399f7230852d3
Mode8fx <gateguy33@gmail.com>
2024-09-08 20:56:38 -0400

    Fixed a description bug + cleanup

--- src/text_options.c
+++ src/text_options.c
@@ -30,7 +30,6 @@ OptionPage optionPage_Empty;
 OptionLine optionPage_Empty_Lines[1];
 
 #define CURR_LINE page->optionLines[lineIndex]
-#define CURR_CHOICE CURR_LINE.optionChoices[choiceIndex]
 
 static void setOptionPageLine(SDL_Renderer *renderer, OptionPage *page, int lineIndex, char text[], Sint8 numChoices, Sint8 choiceIndex, int nextState) {
 	snprintf(CURR_LINE.name.text, TEXT_LINE_SIZE, text);
@@ -46,30 +45,31 @@ static void setOptionPageLine(SDL_Renderer *renderer, OptionPage *page, int line
 }
 
 static void setOptionChoice(SDL_Renderer *renderer, OptionPage *page, int lineIndex, int choiceIndex, char name[], Sint8 numDescLines, char descLine1[], char descLine2[], char descLine3[]) {
-	snprintf(CURR_CHOICE.name.text, TEXT_LINE_SIZE, "%s", name);
-	prepareMessage(renderer, OPTION_FONT, OPTION_OUTLINE_SIZE, &CURR_CHOICE.name, 1, color_white, color_black);
-	setMessagePosRelativeToScreen_LeftAlign(&CURR_CHOICE.name, CHOICE_X, CHOICE_Y);
-	snprintf(CURR_CHOICE.descLine1.text, TEXT_LINE_SIZE, "%s", descLine1);
-	snprintf(CURR_CHOICE.descLine2.text, TEXT_LINE_SIZE, "%s", descLine2);
-	snprintf(CURR_CHOICE.descLine3.text, TEXT_LINE_SIZE, "%s", descLine3);
-	prepareMessage(renderer, OPTION_FONT, OPTION_OUTLINE_SIZE, &CURR_CHOICE.descLine1, 1, color_gray, color_black);
-	prepareMessage(renderer, OPTION_FONT, OPTION_OUTLINE_SIZE, &CURR_CHOICE.descLine2, 1, color_gray, color_black);
-	prepareMessage(renderer, OPTION_FONT, OPTION_OUTLINE_SIZE, &CURR_CHOICE.descLine3, 1, color_gray, color_black);
+	OptionChoice *currChoice = &CURR_LINE.optionChoices[choiceIndex];
+	snprintf(currChoice->name.text, TEXT_LINE_SIZE, "%s", name);
+	prepareMessage(renderer, OPTION_FONT, OPTION_OUTLINE_SIZE, &currChoice->name, 1, color_white, color_black);
+	setMessagePosRelativeToScreen_LeftAlign(&currChoice->name, CHOICE_X, CHOICE_Y);
+	snprintf(currChoice->descLine1.text, TEXT_LINE_SIZE, "%s", descLine1);
+	snprintf(currChoice->descLine2.text, TEXT_LINE_SIZE, "%s", descLine2);
+	snprintf(currChoice->descLine3.text, TEXT_LINE_SIZE, "%s", descLine3);
+	prepareMessage(renderer, OPTION_FONT, OPTION_OUTLINE_SIZE, &currChoice->descLine1, 1, color_gray, color_black);
+	prepareMessage(renderer, OPTION_FONT, OPTION_OUTLINE_SIZE, &currChoice->descLine2, 1, color_gray, color_black);
+	prepareMessage(renderer, OPTION_FONT, OPTION_OUTLINE_SIZE, &currChoice->descLine3, 1, color_gray, color_black);
 	switch (numDescLines) {
 		case 2:
-			setMessagePosRelativeToScreen(&CURR_CHOICE.descLine1, 0.5f, DESC_LINE_Y_POS_2);
-			setMessagePosRelativeToScreen(&CURR_CHOICE.descLine2, 0.5f, DESC_LINE_Y_POS_4);
-			setMessagePosRelativeToScreen(&CURR_CHOICE.descLine3, 0.5f, 0);
+			setMessagePosRelativeToScreen(&currChoice->descLine1, 0.5f, DESC_LINE_Y_POS_2);
+			setMessagePosRelativeToScreen(&currChoice->descLine2, 0.5f, DESC_LINE_Y_POS_4);
+			setMessagePosRelativeToScreen(&currChoice->descLine3, 0.5f, 0);
 			break;
 		case 3:
-			setMessagePosRelativeToScreen(&CURR_CHOICE.descLine1, 0.5f, DESC_LINE_Y_POS_1);
-			setMessagePosRelativeToScreen(&CURR_CHOICE.descLine2, 0.5f, DESC_LINE_Y_POS_3);
-			setMessagePosRelativeToScreen(&CURR_CHOICE.descLine3, 0.5f, DESC_LINE_Y_POS_5);
+			setMessagePosRelativeToScreen(&currChoice->descLine1, 0.5f, DESC_LINE_Y_POS_1);
+			setMessagePosRelativeToScreen(&currChoice->descLine2, 0.5f, DESC_LINE_Y_POS_3);
+			setMessagePosRelativeToScreen(&currChoice->descLine3, 0.5f, DESC_LINE_Y_POS_5);
 			break;
 		default:
-			setMessagePosRelativeToScreen(&CURR_CHOICE.descLine1, 0.5f, DESC_LINE_Y_POS_3);
-			setMessagePosRelativeToScreen(&CURR_CHOICE.descLine2, 0.5f, 0);
-			setMessagePosRelativeToScreen(&CURR_CHOICE.descLine3, 0.5f, 0);
+			setMessagePosRelativeToScreen(&currChoice->descLine1, 0.5f, DESC_LINE_Y_POS_3);
+			setMessagePosRelativeToScreen(&currChoice->descLine2, 0.5f, 0);
+			setMessagePosRelativeToScreen(&currChoice->descLine3, 0.5f, 0);
 			break;
 		}
 }
@@ -81,8 +81,9 @@ void initStaticMessages_Options(SDL_Renderer *renderer) {
 	optionPage_Main.numLines = OPTION_PAGE_MAIN_NUM_LINES;
 	optionPage_Main.optionLines = optionPage_Main_Lines;
 	optionPage_Main.prevState = GAME_STATE_TITLE_SCREEN;
-	setOptionPageLine(renderer, &optionPage_Main, 0, "Game Options", 1, 0, GAME_STATE_OPTIONS);
-	setOptionChoice(renderer, &optionPage_Main, 0, 0, EMPTY, 3, "TEST A", "TEST B", "TEST C");
+	setOptionPageLine(renderer, &optionPage_Main, 0, "Game Options", 2, 0, GAME_STATE_OPTIONS);
+	setOptionChoice(renderer, &optionPage_Main, 0, 0, "TEST ABC", 3, "TEST A", "TEST B", "TEST C");
+	setOptionChoice(renderer, &optionPage_Main, 0, 1, "TEST DEF", 3, "TEST D", "TEST E", "TEST F");
 	setOptionPageLine(renderer, &optionPage_Main, 1, "Visuals", 1, 0, GAME_STATE_OPTIONS);
 	setOptionChoice(renderer, &optionPage_Main, 1, 0, EMPTY, 1, EMPTY, EMPTY, EMPTY);
 	setOptionPageLine(renderer, &optionPage_Main, 2, "Audio", 1, 0, GAME_STATE_OPTIONS);
@@ -124,6 +125,13 @@ void handlePage(SDL_Renderer *renderer, OptionPage *page, bool renderCursor) {
 			setMessagePosRelativeToScreen_LeftAlign(&message_menu_cursor, CURSOR_X, CURSOR_Y);
 		}
 	}
+	if (keyPressed(INPUT_LEFT)) {
+		OptionLine *currentLine = &page->optionLines[page->index];
+		currentLine->index = (currentLine->index - 1 + currentLine->numChoices) % currentLine->numChoices;
+	} else if (keyPressed(INPUT_RIGHT)) {
+		OptionLine *currentLine = &page->optionLines[page->index];
+		currentLine->index = (currentLine->index + 1) % currentLine->numChoices;
+	}
 	if (keyPressed(INPUT_A)) {
 		if (page->optionLines[page->index].nextState != -1) {
 			gameState = page->optionLines[page->index].nextState;
@@ -149,8 +157,9 @@ void handlePage(SDL_Renderer *renderer, OptionPage *page, bool renderCursor) {
 	for (Sint8 lineIndex = 0; lineIndex < page->numLines; lineIndex++) {
 		renderMessage(renderer, &CURR_LINE.name);
 		renderMessage(renderer, &CURR_LINE.optionChoices[CURR_LINE.index].name);
-		renderMessage(renderer, &CURR_LINE.optionChoices[CURR_LINE.index].descLine1);
-		renderMessage(renderer, &CURR_LINE.optionChoices[CURR_LINE.index].descLine2);
-		renderMessage(renderer, &CURR_LINE.optionChoices[CURR_LINE.index].descLine3);
 	}
+	OptionChoice *currChoice = &page->optionLines[page->index].optionChoices[page->optionLines[page->index].index];
+	renderMessage(renderer, &currChoice->descLine1);
+	renderMessage(renderer, &currChoice->descLine2);
+	renderMessage(renderer, &currChoice->descLine3);
 }
